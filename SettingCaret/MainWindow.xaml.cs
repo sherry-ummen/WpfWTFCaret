8dfa2d770015bfda4482b2a716bb2e844d4146a5
Sherry Ummen <sherry.ummen@napa.fi>
2018-08-08 09:34:00 +0300

    Unable to get the caret to correct position

--- SettingCaret/MainWindow.xaml.cs
+++ SettingCaret/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SettingCaret {
     /// <summary>
@@ -20,6 +10,37 @@ namespace SettingCaret {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            CommandLineBox.Text = "\r\nTASK?>";
+        }
+
+        private void CommandLineBox_PreviewKeyDown(object sender, KeyEventArgs e) {
+            var text = GetCurrentLineText();
+            var t = Regex.Replace(text, @"(^[^\?]*\?>)(.*)", $"$1 {Guid.NewGuid().ToString()}");
+            ReplaceCurrentLineText(t);
+        }
+
+        private void CommandLineBox_OnLoaded(object sender, RoutedEventArgs e) {
+            CommandLineBox.Focus();
+            CommandLineBox.CaretIndex = CommandLineBox.Text.Length;
+        }
+
+        private void ReplaceCurrentLineText(string newText) {
+            var caretIndex = CommandLineBox.CaretIndex;
+            var lineIndex = CommandLineBox.GetLineIndexFromCharacterIndex(caretIndex);
+            var characterIndex = CommandLineBox.GetCharacterIndexFromLineIndex(lineIndex);
+            var textOnLine = CommandLineBox.GetLineText(lineIndex);
+            CommandLineBox.Select(characterIndex, textOnLine.Length);
+            CommandLineBox.SelectedText = newText;
+            CommandLineBox.Focus();
+            CommandLineBox.Select(CommandLineBox.Text.Length, 0);
+            CommandLineBox.ScrollToLine(lineIndex);
+        }
+
+        private string GetCurrentLineText() {
+            var caretIndex = CommandLineBox.CaretIndex;
+            var lineIndex = CommandLineBox.GetLineIndexFromCharacterIndex(caretIndex);
+            return lineIndex >= 0 && lineIndex < CommandLineBox.LineCount ?
+                CommandLineBox.GetLineText(lineIndex) : "";
         }
     }
 }
